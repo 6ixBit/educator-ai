b387c11c8906ac37284351da626c7c5cc0d04cc6
Hamza <Hamzacarew@gmail.com>
2023-11-23 20:50:11 -0500

    now get user answer, wrong answer and correct answer

--- app/learn/components/Quiz.tsx
+++ app/learn/components/Quiz.tsx
@@ -8,9 +8,29 @@ interface IQuiz {
 export default function Quiz({ questions }: IQuiz) {
   const [currentOption, setCurrentOption] = useState(0);
   const [radioGrpVal, setRadioGrpVal] = useState<any>(null);
+  const [userAnswers, setUserAnswers] = useState<Array<any>>([]);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [wrongAnswers, setWrongAnswers] = useState<
+    Array<{ question: string; correctAnswer: string; userAnswer: string }>
+  >([]);
 
   const handleNext = () => {
     if (currentOption < questions.length - 1) {
+      setUserAnswers([...userAnswers, radioGrpVal]);
+
+      // TODO: Rework, this looks like shit.
+      if (radioGrpVal === questions[currentOption].correct_answer) {
+        setCorrectAnswers(correctAnswers + 1);
+      } else {
+        setWrongAnswers([
+          ...wrongAnswers,
+          {
+            question: questions[currentOption].question,
+            correctAnswer: questions[currentOption].correct_answer,
+            userAnswer: radioGrpVal,
+          },
+        ]);
+      }
       setCurrentOption(currentOption + 1);
     }
   };
@@ -20,8 +40,6 @@ export default function Quiz({ questions }: IQuiz) {
       setCurrentOption(currentOption - 1);
     }
   };
-
-  console.log("quiz component: ", questions);
 
   return (
     <div className="mt-12 mb-4 w-full">
@@ -49,15 +67,33 @@ export default function Quiz({ questions }: IQuiz) {
         }}
         value={radioGrpVal}
       />
-
-      <div className="flex flex-row justify-center mt-6">
+      <div className="flex flex-row justify-between mt-6 gap-2">
+        <button
+          onClick={() => {
+            handlePrev();
+          }}
+          disabled={currentOption === 0}
+          className={`text-yellow-500 rounded-lg w-20 text-center h-8 ${
+            currentOption === 0 ? "opacity-50" : ""
+          }`}
+        >
+          Prev
+        </button>
+        <button
+          onClick={() => {
+            console.log(wrongAnswers);
+          }}
+          className="bg-green-500 text-white rounded-full w-20 text-center h-8"
+        >
+          Submit
+        </button>
         <button
           onClick={() => {
             handleNext();
             setRadioGrpVal(null);
           }}
           disabled={currentOption === questions.length - 1}
-          className={`bg-blue-500 text-white rounded-lg w-20 text-center h-8 ${
+          className={`border-blue-500 text-blue-500 border-2 rounded-full w-20 text-center h-8 ${
             currentOption === questions.length - 1 ? "opacity-50" : ""
           }`}
         >
@@ -65,11 +101,19 @@ export default function Quiz({ questions }: IQuiz) {
         </button>
       </div>
 
-      {/* {radioGrpVal && (
-        <h1 className="mt-4 text-md text-lime-500">
-          Your choice: {radioGrpVal}
-        </h1>
-      )} */}
+      <h2 className="text-gray-500 text-sm mb-3 mt-2 font-normal">
+        Correct answers: {correctAnswers}
+      </h2>
+      {wrongAnswers.map((item, index) => (
+        <div key={index} className="mt-12">
+          <h2 className="text-red-500 text-sm mb-3 font-normal">
+            Wrong answer for: {item.question}
+          </h2>
+          <h2 className="text-green-500 text-sm mb-3 font-normal">
+            Correct answer is: {item.correctAnswer}
+          </h2>
+        </div>
+      ))}
     </div>
   );
 }
