9a983e55c08e056dc5f3c341a5187c0535f8941f
Hamza <Hamzacarew@gmail.com>
2024-01-05 15:57:06 -0500

    initlal form setup for quiz

--- app/quiz/create/page.tsx
+++ app/quiz/create/page.tsx
@@ -1,23 +1,38 @@
 "use client";
 
+import * as Form from "@radix-ui/react-form";
+
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useRef, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@mui/material";
-import { useRef } from "react";
+import ProgressBar from "@/components/ProgressBar";
+import { useIntl } from "react-intl";
 
 export default function Page() {
+  const intl = useIntl();
+  const [loading, setLoading] = useState(false);
+
   // Create a reference to the hidden file input
   const fileInputRef = useRef(null);
 
   // Function to trigger the file input click event
   const handleButtonClick = () => {
     fileInputRef.current.click();
+  };
+
+  const handleSubmit = () => {
+    console.log("Form submitted with raw text.");
+  };
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    console.log(event.target.value);
   };
 
   return (
@@ -49,6 +64,37 @@ export default function Page() {
             ref={fileInputRef}
           />
         </div>
+
+        <Form.Root
+          className="w-10/12 sm:w-[500px] md:w-[500px] lg:w-[685px] mt-6"
+          onSubmit={handleSubmit}
+        >
+          <Form.Field className=" mb-[10px]" name="content">
+            <Form.Control asChild>
+              <textarea
+                className="box-border h-72 sm:h-80 text-black w-full p-6 sm:w-full shadow-blackA6 inline-flex appearance-none items-center justify-center rounded-[4px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA6 resize-none"
+                placeholder="How did the Roman empire become so dominant?"
+                required
+                disabled={loading}
+                onChange={handleTextChange}
+              />
+            </Form.Control>
+          </Form.Field>
+
+          <Form.Submit asChild>
+            <div className="flex justify-center">
+              {loading ? (
+                <div className="w-9/12 mt-2">
+                  <ProgressBar initialProgress={10} end={!loading} />
+                </div>
+              ) : (
+                <Button variant="submit_blue" disabled={loading}>
+                  {intl.formatMessage({ id: "button.generate.quiz" })}
+                </Button>
+              )}
+            </div>
+          </Form.Submit>
+        </Form.Root>
       </Card>
     </div>
   );
